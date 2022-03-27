6d4885624002884ca1a8aa26125d22d079e92c6c
ruturajnawale10 <ruturajnawale99@gmail.com>
2022-03-26 22:41:43 -0700

    Added login and register components.

---
+++ frontend/src/components/Register.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import "../App.css";
+import axios from "axios";
+import cookie from "react-cookies";
+import { Redirect } from "react-router";
+
+//Define a Register Component
+function Register() {
+  //maintain the state required for this component
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [authMsg, setAuthMsg] = useState(null);
+  const [RedirectVar, setRedirectVar] = useState(null);
+
+  //currently using cookie, todo: use JWT token
+  let RedirectVar1 = null;
+  if (cookie.load("cookie")) {
+    RedirectVar1 = <Redirect to="/home" />;
+  }
+
+  //submit Login handler to send a request to the node backend
+  const submitRegister = (e) => {
+    //prevent page from refresh
+    e.preventDefault();
+    const data = {
+      email: email,
+      password: password,
+    };
+    //set the with credentials to true
+    axios.defaults.withCredentials = true;
+    //make a post request with the user data
+    axios
+      .post(process.env.REACT_APP_LOCALHOST + "/register", data)
+      .then((response) => {
+        if (response.data === "SUCCESS") {
+          setRedirectVar(<Redirect to="/login" />);
+        } else {
+          setAuthMsg(
+            <p style={{ color: "red" }}>
+              An account with this email already exists!
+            </p>
+          );
+        }
+      });
+  };
+
+  return (
+    <div>
+      {RedirectVar}
+      {RedirectVar1}
+      <form onSubmit={submitRegister}>
+        <div class="container">
+          <div class="login-form">
+            <div class="main-div">
+              <div class="panel">
+                <h2>Register</h2>
+                <p>Please enter your email and password</p>
+              </div>
+
+              <div class="form-group">
+                <input
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  type="email"
+                  class="form-control"
+                  name="email"
+                  placeholder="Email"
+                  autoFocus
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  type="password"
+                  class="form-control"
+                  name="password"
+                  placeholder="Password"
+                />
+              </div>
+              <button type="submit" class="btn btn-dark">
+                Register
+              </button>
+              {authMsg}
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+//export Register Component
+export default Register;

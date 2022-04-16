1fda1ec43a2b97c635af097b6597a98e0149784f
Roshan <chokshiroshan@gmail.com>
2022-04-15 17:16:51 -0700

    Added Hoteloverview.

--- frontend/src/components/Hotelcard.js
+++ frontend/src/components/Hotelcard.js
@@ -1,32 +1,38 @@
 import React from "react";
 import hotelUrl from "../images/hotel.jpg";
+import { Link } from "react-router-dom";
 
 function Hotelcard(props) {
-
   return (
     <div class="container">
       <div class="row">
-              <div class="col-md-4">
-                <img
-                  src={hotelUrl}
-                  alt="Unavailable"
-                  style={{ width: "100%" }}   
-                ></img>
-                </div>
+        <div class="col-md-4">
+          <img src={hotelUrl} alt="Unavailable" style={{ width: "100%" }}></img>
+        </div>
 
-                <div class="col-md-4" style={{marginLeft:"50px", marginTop: "50px"}}>
-                <div >
-                  <div style={{ display: "inline-block" }}>
-                   <h2>{props.hotel.hotelName}</h2> 
-                   <p style= {{fontSize:"20px"}}> Location: {props.hotel.hotelAddress}</p>
-                  </div>
+        <div class="col-md-4" style={{ marginLeft: "50px", marginTop: "50px" }}>
+          <div>
+            <div style={{ display: "inline-block" }}>
+              <h2>{props.hotel.hotelName}</h2>
+              <p style={{ fontSize: "20px" }}>
+                {" "}
+                Location: {props.hotel.hotelAddress}
+              </p>
+            </div>
 
-                  <div style={{ display: "inline-block", marginLeft: "100px" }}>
-                    <button class="btn btn-dark" onClick={() => window.location.href=`/hotels/${props.hotel.hotelName}`}>View Rates</button>
-                  </div>
-                  </div>
-               
-                </div>
+            <div style={{ display: "inline-block", marginLeft: "100px" }}>
+              <Link
+                class="btn btn-dark"
+                to={"/hoteloverview"}
+                state={props.hotel}
+              >
+                View Rates
+              </Link>
+
+              {/* <button class="btn btn-dark" onClick={() => window.location.href=`/hotels/${props.hotel.hotelName}`}>View Rates</button> */}
+            </div>
+          </div>
+        </div>
       </div>
       <br></br>
     </div>
--- frontend/src/components/Hoteloverview.js
+++ frontend/src/components/Hoteloverview.js
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import Roomcard from "./Roomcard";
 
-function Hoteloverview(props) {
-  let url = document.location.href;
-  let url_arr = url.split("/");
-  let hotelName = url_arr.pop();
-  hotelName = hotelName.replaceAll("%20", " ").trim();
-
+function Hoteloverview() {
+  const location = useLocation();
+  const hotel = location?.state;
+  console.log(hotel);
   return (
-    <div>
-    {hotelName}
-    Hello hotel page
-    This is change from RN branch
-      {hotelName}
-     
+    <div className="container m-3">
+      <div className="row offset-md-4">
+        <div class="col-md-8 offset-md-4">
+          <h1>{hotel.hotelName}</h1>
+          <h3>{hotel.hotelAddress}</h3>
+          <p>{hotel.hotelPhone}</p>
+        </div>
+        {hotel.rooms.map((room) => (
+          <div key={room._id} id="cardItem" className="col-md-8">
+            <Roomcard room={room} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }

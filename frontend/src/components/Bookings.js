44ee506eebc0f94fae7a730ae5df7bdd480aa4fd
ruturajnawale10 <ruturajnawale99@gmail.com>
2022-05-11 01:14:34 -0700

    - Displayed basic past bookings details  of the user

--- frontend/src/components/Bookings.js
+++ frontend/src/components/Bookings.js
@@ -1,39 +1,46 @@
 import { React, useEffect, useState } from "react";
 import axios from "axios";
-import Hotelcard from "./Hotelcard";
+import Bookingcard from "./Bookingcard";
 import { useNavigate } from "react-router-dom";
 
 function Bookings() {
-  const [hotels, setHotels] = useState([]);
+  const [bookings, setbookings] = useState([]);
   const [RedirectVar, setRedirectVar] = useState(null);
   let navigate = useNavigate();
+  let userName = localStorage.getItem("userName");
 
   useEffect(() => {
-    // if (!localStorage.getItem("userName")) {
-    //   setRedirectVar(navigate("../users/login", { replace: true }));
-    // }
-    // axios
-    //   .get(process.env.REACT_APP_LOCALHOST + "/hotels/gethotels")
-    //   .then((response) => {
-    //     console.log(response.data);
-    //     setHotels(
-    //       <div className="container">
-    //         <div className="row">
-    //           {response.data.map((hotel) => (
-    //             <div key={hotel.hotelName} id="cardItem" className="col-xs-4">
-    //               <Hotelcard hotel={hotel} />
-    //             </div>
-    //           ))}
-    //         </div>
-    //       </div>
-    //     );
-    //   });
+    if (!localStorage.getItem("userName")) {
+      setRedirectVar(navigate("../users/login", { replace: true }));
+    }
+    axios
+      .get(process.env.REACT_APP_LOCALHOST + "/users/mybookings", {
+        params: { username: userName },
+      })
+      .then((response) => {
+        setbookings(
+          <div className="container">
+            <div className="row">
+              {response.data.map((booking) => (
+                <div
+                  key={booking.bookingName}
+                  id="cardItem"
+                  className="col-xs-4"
+                >
+                  <Bookingcard booking={booking} />
+                </div>
+              ))}
+            </div>
+          </div>
+        );
+      });
   }, []);
 
   return (
     <div class="home">
       {RedirectVar}
-      <h1>Past Bookings Page</h1>
+      <h1>Bookings</h1>
+      {bookings}
     </div>
   );
 }
